280cdd3dee9374bae65b384a984e9bcbbb8f9e55
Yaisa Ramirez <yaisa.ramirez@gmail.com>
2021-11-04 19:37:31 -0500

    codigo sistema extraccion

--- ros_extraccion.py
+++ ros_extraccion.py
@@ -9,22 +9,20 @@ from smbus2 import SMBus
 
 topic_sistema_extraccion_b = '/robocol/interfaz/brazo/sistema_extraccion_b'  #creacion topico
 
-# def mandar_i2c(data):
-
-#     with SMBus(1) as bus:
-#         address = 0x24  #direccion arduino
-#         bus.write_byte(address, data)
-#         time.sleep(0.2)
+def mandar_i2c(data):
+     with SMBus(1) as bus:
+         address = 0x24  #direccion arduino
+         bus.write_byte(address, data)
+         time.sleep(0.2)
         
-#         var = bus.read_byte(0x25)
-#         print(var)
+         var = bus.read_byte(0x25)
+         print(var)
               
 
 def callback(data):
     print("Mensaje recibido del topico del sistema de extraccion B")
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    # await self.send(text_data=json.dumps({'id': 'sistema_extraccion_b', "orden": param.data}))
-    #mandar_i2c(data)
+    mandar_i2c(data)
     
     
     
